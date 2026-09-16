0e3796e9923dc9a1acb626fcb3257d6c5bb099a6
Joseph Indra <josephindraa@gmail.com>
2026-09-16 22:18:41 +0700

    Add base config and custom module

---
+++ addons/cx_base/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Core Base Foundation',
+    'summary': 'Foundation for global defaults, business overrides, and core contracts',
+    'author': 'Joseph Indra <josephindraa@gmail.com>',
+    'license': 'LGPL-3',
+    'version': '1.0.0',
+    'depends': ['base'],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+}
